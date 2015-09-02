783d9e3f0427d1d1d9869fbdf8689557ac3d7e16
root <root@localhost.localdomain>
2015-09-02 07:35:18 -0600

    jose

--- web/Matematica/index.php
+++ web/Matematica/index.php
@@ -1,1 +1,66 @@
+<?php
 
+require_once("clase/usuario.php");
+
+?>
+
+
+
+<html>
+ 
+  <head>
+   
+  <title> chikipagina 2015</title>
+  <link href="imagene/icono.ico" type="image/x-icon" rel="shortcut icon">
+  
+  <style type="text/css">
+   .styled-button-1 {
+	background: #25A6E1;
+	background: -moz-linear-gradient(top,#25A6E1 0%,#188BC0 100%);
+	background: -webkit-gradient(linear,left top,left bottom,color-stop(0%,#25A6E1),color-stop(100%,#188BC0));
+	background: -webkit-linear-gradient(top,#25A6E1 0%,#188BC0 100%);
+	background: -o-linear-gradient(top,#25A6E1 0%,#188BC0 100%);
+	background: -ms-linear-gradient(top,#25A6E1 0%,#188BC0 100%);
+	background: linear-gradient(top,#25A6E1 0%,#188BC0 100%);
+	filter: progid: DXImageTransform.Microsoft.gradient( startColorstr='#25A6E1',endColorstr='#188BC0',GradientType=0);
+	padding:8px 13px;
+	color:#fff;
+	font-family:'Helvetica Neue',sans-serif;
+	font-size:17px;
+	border-radius:4px;
+	-moz-border-radius:4px;
+	-webkit-border-radius:4px;
+	height:100%;
+	width: 100%;
+	border:1px solid #1A87B9
+
+}
+ </style>
+  
+
+</head>
+<body>
+ <body>
+
+<div  style = "display: table; margin-left: auto; margin-right: auto; width: 90%; height: 100%; background-color:yellow;"> 
+    <div style = "height: 30%; width: 100%; background-color:white; background-image:url('imagene/portada.jpg'); background-repeat: no-repeat; background-position: center top;  border-style: solid; border-color: #ff0000 #0000ff ;"> </div>
+    <div style = "float: left; height: 60%; width: 30%; background-color:red;"> 
+       <div style = "clear: both; height: 50%; width: 100%; background-color:teal"> 
+           <div  style = "float: left;  margin-left: 20px;   margin-top: 20px;margin-bottom: 10px; height: 50%; width: 50%; background-color:blue; ">  hola
+       </div>   
+        <div  style = "clear: both;  margin-top: 20px; margin-right: 40px;  margin-left: 40px; height: 20%; width: 80%; background-color:red">  hola
+       </div>       
+       
+       </div>
+       <div style = "clear: both; height: 50%; width: 100%; background-color:blue"> actividades </div>    
+     </div>
+    <div style = "float: right; height: 60%; width: 70%; background-color:silver;">
+    
+     <div style = "clear: both; height: 10%; width: 100%; background-color:red"> <div style = "float: left; height: 100%; width: 20%; background-color:blue"> <input type="submit" class="styled-button-1" value="Anime" onclick="window.location='Clase/conectar.php'"  />   </div>  <div style = "float: left; height: 100%; width: 20%; background-color:yellow"> <input type="submit" class="styled-button-1" value="Musica" />  </div>  <div style = "float: left; height: 100%; width: 20%; background-color:blue">  <input type="submit" class="styled-button-1" value="Programacion" />   </div> <div style = "float: left; height: 100%; width: 20%; background-color:yellow"> <input type="submit" class="styled-button-1" value="Matematica" />   </div> <div style = "float: left; height: 100%; width: 20%; background-color:blue"><input type="submit" class="styled-button-1" value="Registrarse" /> </div>                 </div>    
+    
+    </div>             
+    
+    <div style = "clear: both; height: 10%; width: 100%; background-color:teal">dfv</div>
+</div> 
+</body>
+</html>
