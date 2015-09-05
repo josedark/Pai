131aa1b6231101728069824c8cabc972b036df41
root <root@localhost.localdomain>
2015-09-05 05:28:44 -0600

    ha

--- web/index.php
+++ web/index.php
@@ -171,7 +171,7 @@ float: center;
                  <img src="imagene/avatar.jpg">
 		      </div>
 		      
-		      <form action="login/login.php" method="POST">
+		      <form action="" method="get">
 		         <div style="width: 100%; height: 16px;">  </div>
 	           	<input type="text" placeholder="username" name="textusuario">
 		         <div class="bar">  </div>
