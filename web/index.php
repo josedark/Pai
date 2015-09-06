e0bec8789457c12a0a79f7d9ae1a5d8eac0173f0
josedark <josedark123456789@gmail.com>
2015-09-05 21:08:50 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -171,7 +171,7 @@ float: center;
                  <img src="imagene/avatar.jpg">
 		      </div>
 		      
-		      <form method="get"  action="login.php">
+		      <form method="get"  action="login/pagina.php">
 		         <div style="width: 100%; height: 16px;">  </div>
 	           	<input type="text" placeholder="username" name="textusuario">
 		         <div class="bar">  </div>
