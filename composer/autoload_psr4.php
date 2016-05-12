f3141b3af5759a767c77df01a5b0b5823b161a1f
jose alvarado <josedark123456789@gmail.com>
2016-05-12 00:43:58 -0600

    Update autoload_psr4.php

--- composer/autoload_psr4.php
+++ composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
+    'Facebook\\' => array($vendorDir . '/Facebook/Facebook/src/Facebook'),
 );
