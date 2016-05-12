39d40f208d6701626f84e3b892cf5ac9d1ab1bc1
jose alvarado <josedark123456789@gmail.com>
2016-05-11 23:13:28 -0600

    Update login.php

--- web/Facebook/login.php
+++ web/Facebook/login.php
@@ -1,6 +1,16 @@
 <?php 
 error_reporting(E_ALL);
 ini_set("display_errors", 1); 
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+
 $fb = new Facebook\Facebook([
   'app_id' => '1691216854434231',
   'app_secret' => 'ebfbfdeabe52074debb3c141d69d2e12',
